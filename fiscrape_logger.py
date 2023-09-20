2e302e193c4a6efc521333eb55a74ac64f9a674b
fvtdotgit <finnvtran@gmail.com>
2023-09-20 05:09:20 -0400

    Also, forgot to upload the logger file...

---
+++ fiscrape_logger.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Set up handler
+stream_handler = logging.StreamHandler()
+file_handler = logging.FileHandler('fiscrape_logging.log')
+
+# Setting up the level
+stream_handler.setLevel(logging.WARNING)
+file_handler.setLevel(logging.INFO)
+
+# Formatting the log outputs
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+stream_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+# Adding the handlers to the logger
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
